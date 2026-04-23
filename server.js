0145aea98b832bab15c4cb7f0ba3beebfad1a347
Gokul Basumatary <gokulbasu1043@gmail.com>
2026-04-24 00:28:46 +0530

    port fix for render

--- server.js
+++ server.js
@@ -77,6 +77,8 @@ app.get("/", (req, res) => {
 });
 
 // Server
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
